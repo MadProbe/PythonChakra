1a303b75051a02a5c0190f4b5987ec6e6db87bf7
MadProbe <49519179+MadProbe@users.noreply.github.com>
2021-05-06 23:15:53 +0300

    Add more package info in __init__.py

--- python_chakra/__init__.py
+++ python_chakra/__init__.py
@@ -1,3 +1,7 @@
-from .index import *  # noqa
+from .index import *  # noqa: F401
 
+__title__ = "python_chakra"
+__author__ = "MadProbe"
+__licence__ = "MIT"
+__copyright__ = "Copyright 2021 - present MadProbe"
 __version__ = "0.0.1"
