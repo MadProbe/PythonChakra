168cd67255531b214148ae541cc4a3b99ed13cda
MadProbe <49519179+MadProbe@users.noreply.github.com>
2021-07-01 20:27:19 +0300

    move CC dll to ./utils/libs

--- python_chakra/utils/init.py
+++ python_chakra/utils/init.py
@@ -1,8 +1,9 @@
 from ctypes import CDLL
 from os import name
+from os.path import dirname
 
 __all__ = 'chakra_core',
 if name == "nt":
-    chakra_core = CDLL("./ChakraCore.dll")
+    chakra_core = CDLL(f"{dirname(__file__)}/libs/ChakraCore.dll")
 elif name == "posix":
-    chakra_core = CDLL("./libChakraCore.dylib")
+    chakra_core = CDLL(f"{dirname(__file__)}/libs/libChakraCore.dylib")
